3d3914d973a245abba6b8824a77ece661fa2e97c
Piotr Siupa <piotr.siupa+karnaugh@gmail.com>
2023-10-28 22:05:49 +0200

    Made a real feature-rich progress bar
    It's good.

---
+++ src/Progress.cc
@@ -0,0 +1,138 @@
+#include "./Progress.hh"
+
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+
+
+Progress::calcSubstepCompletion_t Progress::calc0SubstepCompletion = [](){ return 0.0; };
+
+Progress::steps_t Progress::calcConservativeStepsToSkip(const double secondsToSkip, const double secondsPerStep) const
+{
+	static constexpr double initialErrorMargin = 16.0, finalErrorMargin = 2.0;
+	static steps_t substepsToReachFinalErrorMargin = 128;
+	// Error margin will slowly drop, making the the result a lot more conservative at the beginning than later on.
+	const double errorMargin = substepsSoFar <= substepsToReachFinalErrorMargin
+		? initialErrorMargin / ((static_cast<double>(substepsSoFar) / static_cast<double>(substepsToReachFinalErrorMargin) * (initialErrorMargin / finalErrorMargin - 1.0)) + 1.0)
+		: finalErrorMargin;
+	const steps_t substepsToSkip = secondsToSkip / secondsPerStep / errorMargin;
+	return std::max<steps_t>(1, substepsToSkip);
+}
+
+bool Progress::checkReportInterval(const bool force)
+{
+	const timePoint_t currentTime = std::chrono::steady_clock::now();
+	const double secondsSinceStart = std::chrono::duration<double>(currentTime - startTime).count();
+	const double secondsSinceLastReport = std::chrono::duration<double>(currentTime - lastReportTime).count();
+	const double remainingSeconds = reportInterval - secondsSinceLastReport;
+	const double secondsPerStep = secondsSinceStart / substepsSoFar;
+	if (secondsSinceStart < 1.0 || (!force && remainingSeconds > secondsPerStep)) // Not even `force` can show progress before 1 second has passed. This is by design.
+	{
+		substepsToSkip = calcConservativeStepsToSkip(remainingSeconds, secondsPerStep);
+		return false;
+	}
+	else
+	{
+		lastReportTime = currentTime;
+		substepsToSkip = calcConservativeStepsToSkip(reportInterval, secondsPerStep);
+		return true;
+	}
+}
+
+void Progress::printTime(double time)
+{
+	std::uintmax_t x = time;
+	const std::uint_fast8_t seconds = x % 60;
+	x /= 60;
+	time -= x * 60 + seconds / 10 * 10;
+	const std::uint_fast8_t minutes = x % 60;
+	x /= 60;
+	const uint_fast8_t hours = x % 24;
+	x /= 24;
+	const uint_fast8_t days = x % 365;
+	x /= 365;
+	const uintmax_t years = x;
+	if (years != 0)
+	{
+		std::clog << static_cast<unsigned>(years) << " year";
+		if (years > 1)
+			std::clog << 's';
+		std::clog << ' ';
+	}
+	if (days != 0)
+	{
+		std::clog << static_cast<unsigned>(days) << " day";
+		if (days > 1)
+			std::clog << 's';
+		std::clog << ' ';
+	}
+	std::clog << std::setfill('0')
+			<< std::setw(2) << static_cast<unsigned>(hours) << ':'
+			<< std::setw(2) << static_cast<unsigned>(minutes) << ':'
+			<< static_cast<unsigned>(seconds / 10) << std::setprecision(3) << time;
+}
+
+void Progress::clearReport()
+{
+	if (reportVisible)
+	{
+		std::clog << "\033[3A\r\033[J";
+		reportVisible = false;
+	}
+}
+
+void Progress::reportProgress(const calcSubstepCompletion_t &calcSubstepCompletion)
+{
+	const completion_t completion = calcSubstepCompletion() / allSteps + calcStepCompletion();
+	
+	clearReport();
+	
+	std::clog << processName;
+	if (allSteps != 1)
+		std::clog << ' ' << '(' << stepsSoFar << '/' << allSteps << ')';
+	for (const auto &subtaskName : subtaskNames)
+		std::clog << " -> " << subtaskName;
+	std::clog << "...\n";
+	
+	const std::uint_fast8_t progressBarLenght = 78 * completion;
+	std::clog << '[' << std::setfill('#') << std::setw(progressBarLenght) << "" << std::setfill('.') << std::setw(78 - progressBarLenght) << "" << ']' << '\n';
+	
+	const timePoint_t currentTime = std::chrono::steady_clock::now();
+	const double secondsSinceStart = std::chrono::duration<double>(currentTime - startTime).count();
+	std::ios oldClogState(nullptr);
+	oldClogState.copyfmt(std::clog);
+	std::clog << std::fixed << "Estimated completion: " << std::setprecision(5) << completion * 100.0 << "%  ET: ";
+	printTime(secondsSinceStart);
+	std::clog << "  ETA: ";
+	if (secondsSinceStart < 0.1 || completion == 0.0)
+	{
+		std::clog << "N/A";
+	}
+	else
+	{
+		const double estimatedTime = secondsSinceStart / completion * (1.0 - completion);
+		printTime(estimatedTime);
+	}
+	std::clog << std::endl;
+	std::clog.copyfmt(oldClogState);
+	
+	reportVisible = true;
+}
+
+void Progress::handleStep(const calcSubstepCompletion_t &calcSubstepCompletion, const bool force)
+{
+	if (checkReportInterval(force))
+		reportProgress(calcSubstepCompletion);
+}
+
+Progress::Progress(const char processName[], const steps_t allSteps) :
+	processName(processName),
+	allSteps(allSteps)
+{
+	if (::terminalStderr)
+	{
+		lastReportTime = startTime = std::chrono::steady_clock::now();
+		substepsToSkip = 2;
+	}
+}
---
+++ src/Progress.hh
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <vector>
+
+#include "global.hh"
+
+
+class Progress
+{
+public:
+	using steps_t = std::uintmax_t;
+	using completion_t = double;
+	using calcSubstepCompletion_t = std::function<completion_t()>;
+	
+	static calcSubstepCompletion_t calc0SubstepCompletion;
+	
+private:
+	using timePoint_t = std::chrono::steady_clock::time_point;
+	
+	static constexpr double reportInterval = 1.0;
+	
+	const char *const processName;
+	timePoint_t startTime, lastReportTime;
+	steps_t allSteps;
+	steps_t stepsSoFar = 0;
+	steps_t substepsSoFar = 0, substepsToSkip;
+	std::vector<const char*> subtaskNames;
+	bool reportVisible = false;
+	
+	steps_t calcConservativeStepsToSkip(const double secondsToSkip, const double secondsPerStep) const;
+	bool checkReportInterval(const bool force);
+	static void printTime(const double seconds);
+	void clearReport();
+	void reportProgress(const calcSubstepCompletion_t &calcSubstepCompletion);
+	void handleStep(const calcSubstepCompletion_t &calcSubstepCompletion, const bool force);
+	
+	completion_t calcStepCompletion() const { return static_cast<completion_t>(stepsSoFar - 1) / allSteps; }
+	
+public:
+	class SubtaskGuard
+	{
+		Progress &progress;
+		friend class Progress;
+		SubtaskGuard(Progress &progress) : progress(progress) {}
+	public:
+		SubtaskGuard(const SubtaskGuard&) = delete;
+		SubtaskGuard& operator=(const SubtaskGuard&) = delete;
+		~SubtaskGuard() { progress.subtaskNames.pop_back(); }
+	};
+	
+	Progress(const char processName[], const steps_t allSteps);
+	Progress(const Progress&) = delete;
+	Progress& operator=(const Progress&) = delete;
+	~Progress() { clearReport(); }
+	
+	void step(const bool force = false) { if (::terminalStderr) { ++stepsSoFar; handleStep(calc0SubstepCompletion, force); } }
+	void substep(const calcSubstepCompletion_t &calcSubstepCompletion, const bool force = false) { if (::terminalStderr) { if (--substepsToSkip == 0 || force) handleStep(calcSubstepCompletion, force); ++substepsSoFar; } }
+	
+	SubtaskGuard enterSubtask(const char subtaskName[]) { subtaskNames.push_back(subtaskName); return {*this}; }
+};
