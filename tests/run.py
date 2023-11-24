5e7788630f4e3e92a889b9b29146e352fde9e01f
Piotr Siupa <piotr.siupa+karnaugh@gmail.com>
2023-11-24 20:06:11 +0100

    Made `tests/run.py` print only failed tests
    It also have proper args parsing now and it can take flag `--verbose` to
    show all the tests as before.

--- tests/run.py
+++ tests/run.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
 import sys
 from pathlib import Path
 import signal
 import subprocess
 import time
+
+
+def parse_args() -> tuple[Path, Path, bool]:
+    parser = argparse.ArgumentParser(
+            prog='run.py',
+            description='This script runs all the tests and checks whether they pass.')
+    parser.add_argument('program')
+    parser.add_argument('-v', '--verbose', action='store_true', help='show all tests even when they pass')
+    args = parser.parse_args()
+    return Path(args.program), args.verbose
 
 
 def find_test_dirs(test_dir: Path) -> list[str]:
@@ -23,43 +34,49 @@ def time_process(process: subprocess.Popen):
     return endtime - starttime
 
 
-def run_test(test_name: str, program: Path, input_file: Path, output_file: Path):
+def run_test(test_name: str, program: Path, input_file: Path, output_file: Path, show_all: bool):
     options = output_file.name.split('_')
-    print(f'Running "{test_name}" ({" ".join(options)})...', end=' ', flush=True)
+    if show_all:
+        print(f'Running "{test_name}" ({" ".join(options)})...', end=' ', flush=True)
     process = subprocess.Popen(['./' + str(program), '--no-status', '--name', test_name] + options + [input_file], text=True, stdout=subprocess.PIPE)
     elapsed_time = time_process(process)
     if process.returncode != 0:
+        if not show_all:
+            print(f'Test "{test_name}" ({" ".join(options)})', end=' ')
         print(f'FAIL ({elapsed_time:.2f}s, return code is {process.returncode})')
         return False
     with open(output_file, 'r') as f:
         expected_output = f.read()
     if process.stdout.read() != expected_output:
+        if not show_all:
+            print(f'Test "{test_name}" ({" ".join(options)})', end=' ')
         print(f'FAIL ({elapsed_time:.2f}s)')
         return False
-    print(f'SUCCESS ({elapsed_time:.2f}s)')
+    if show_all:
+        print(f'SUCCESS ({elapsed_time:.2f}s)')
     return True
 
 
-def run_tests(main_test_dir: Path, program: Path, test_dir: Path) -> bool:
+def run_tests(main_test_dir: Path, program: Path, test_dir: Path, show_all: bool) -> bool:
     input_file = test_dir / 'input'
     output_files = sorted(output_file for output_file in test_dir.iterdir() if output_file != input_file)
     success_count = 0
     for output_file in output_files:
-        if run_test(test_dir.name, program, input_file, output_file):
+        if run_test(test_dir.name, program, input_file, output_file, show_all):
             success_count += 1
     return success_count, len(output_files)
 
 
 def main() -> None:
     main_test_dir = Path(sys.argv[0]).parent
-    program = Path(sys.argv[1])
+    program, show_all = parse_args()
     
     test_dirs = find_test_dirs(main_test_dir)
     
     success_count = 0
     test_count = 0
     for test_dir in test_dirs:
-        this_success_count, this_test_count = run_tests(main_test_dir, program, test_dir)
+        this_success_count, this_test_count = run_tests(main_test_dir, program, test_dir, show_all)
         success_count += this_success_count
         test_count += this_test_count
     print(f'Passed {success_count}/{test_count} tests.')
