e74bc30de1427e94057e8cb1c133a8e910e22744
Piotr Siupa <piotr.siupa+karnaugh@gmail.com>
2023-12-09 01:22:05 +0100

    Made some key functions inline
    This gives a 6% speed gain for processing input stream on GCC.

--- src/Input.hh
+++ src/Input.hh
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <stdexcept>
+#include <algorithm>
+#include <cctype>
 #include <istream>
+#include <stdexcept>
 #include <string>
 
 #include "Progress.hh"
@@ -28,12 +30,144 @@ public:
 	
 	static void recomputeMintermSize() { maxMintermForMultiplying = ::maxMinterm / 10; }
 	
-	[[nodiscard]] bool hasNext(Progress *const progress = nullptr);
-	[[nodiscard]] bool hasNextInLine(Progress *const progress = nullptr);
-	[[nodiscard]] bool isNextText() const;
-	[[nodiscard]] bool doesNextStartWithDash() const;
-	[[nodiscard]] std::string getLine(Progress *const progress = nullptr);
-	[[nodiscard]] std::string getWord(Progress *const progress = nullptr);
-	[[nodiscard]] Minterm getMinterm(Progress &progress);
-	[[nodiscard]] bool hasError() const;
+	[[nodiscard]] inline bool hasNext(Progress *const progress = nullptr);
+	[[nodiscard]] inline bool hasNextInLine(Progress *const progress = nullptr);
+	[[nodiscard]] inline bool isNextText() const;
+	[[nodiscard]] inline bool doesNextStartWithDash() const;
+	[[nodiscard]] inline std::string getLine(Progress *const progress = nullptr);
+	[[nodiscard]] inline std::string getWord(Progress *const progress = nullptr);
+	[[nodiscard]] inline Minterm getMinterm(Progress &progress);
+	[[nodiscard]] inline bool hasError() const { return istream.bad(); }
 };
+
+char Input::getChar(Progress *const progress)
+{
+	char c;
+	istream.get(c);
+	if (!istream)
+	{
+		if (istream.eof())
+			return '\0';
+		throwInputError(progress);
+	}
+	return c;
+}
+
+bool Input::hasNext(Progress *const progress)
+{
+	while (true)
+	{
+		if (firstChar == '\0')
+		{
+			return false;
+		}
+		else if (firstChar == '#')
+		{
+			while (true)
+			{
+				firstChar = getChar(progress);
+				if (firstChar == '\0')
+					return false;
+				if (firstChar == '\n')
+					break;
+			}
+		}
+		else if (!std::isspace(firstChar) && (firstChar == '-' || firstChar == '_' || !std::ispunct(firstChar)))
+		{
+			return true;
+		}
+		firstChar = getChar(progress);
+	}
+}
+
+bool Input::hasNextInLine(Progress *const progress)
+{
+	while (true)
+	{
+		if (firstChar == '\0' || firstChar == '\n')
+			return false;
+		if (!std::isspace(firstChar) && (firstChar == '-' || firstChar == '_' || !std::ispunct(firstChar)))
+			return true;
+		firstChar = getChar(progress);
+	}
+}
+
+bool Input::isNextText() const
+{
+	return firstChar == '_' || std::isalpha(firstChar);
+}
+
+bool Input::doesNextStartWithDash() const
+{
+	return firstChar == '-';
+}
+
+std::string Input::getLine(Progress *const progress)
+{
+	std::string line(1, firstChar);
+	char c;
+	while (true)
+	{
+		c = getChar(progress);
+		if (c == '\n' || c == '\0')
+			break;
+		line += c;
+	}
+	firstChar = c;
+    line.erase(std::find_if(line.rbegin(), line.rend(), [](unsigned char ch) { return !std::isspace(ch); }).base(), line.end());
+	return line;
+}
+
+std::string Input::getWord(Progress *const progress)
+{
+	std::string word(1, firstChar);
+	char c;
+	while (true)
+	{
+		c = getChar(progress);
+		if (std::isspace(c) || (c != '-' && c != '_' && std::ispunct(c)) || c == '\0')
+			break;
+		word += c;
+	}
+	firstChar = c;
+	return word;
+}
+
+Minterm Input::getMinterm(Progress &progress)
+{
+	Minterm minterm = 0;
+	char c = firstChar;
+	while (true)
+	{
+		if (!std::isdigit(c))
+		{
+			progress.cerr() << '"' << c << "\" is not a digit!\n";
+			throw Error("not a digit");
+		}
+		if (minterm > maxMintermForMultiplying)
+		{
+			too_big:
+			auto cerr = progress.cerr();
+			cerr << '"' << minterm;
+			while (!std::isspace(c) && (c == '-' || c == '_' || !std::ispunct(c)) && c != '\0')
+			{
+				cerr << c;
+				c = getChar(&progress);
+			}
+			cerr << "\" is too big!\n";
+			throw Error("number too big");
+		}
+		minterm *= 10;
+		if (::maxMinterm - minterm < static_cast<Minterm>(c - '0'))
+		{
+			minterm /= 10;
+			goto too_big;
+		}
+		minterm += c - '0';
+		c = getChar(&progress);
+		if (std::isspace(c) || (c != '-' && c != '_' && std::ispunct(c)) || c == '\0')
+			break;
+	}
+	firstChar = c;
+	return minterm;
+}
