fc03f78e362570e86bd1c91ba9794233881cefb5
Piotr Siupa <piotr.siupa+karnaugh@gmail.com>
2023-12-15 18:26:48 +0100

    An option in the test script to regenerate outputs

--- tests/run.py
+++ tests/run.py
@@ -16,8 +16,9 @@ def parse_args() -> tuple[Path, Path, bool]:
     parser.add_argument('program')
     parser.add_argument('-v', '--verbose', action='store_true', help='show all tests even when they pass')
     parser.add_argument('-d', '--diff', action='store_true', help='show diff between expected and real outputs')
+    parser.add_argument('-r', '--regenerate', action='store_true', help='save the results instead of checking them')
     args = parser.parse_args()
-    return Path(args.program), args.verbose, args.diff
+    return Path(args.program), args.verbose, args.diff, args.regenerate
 
 
 def find_test_dirs(test_dir: Path) -> list[str]:
@@ -40,7 +41,7 @@ def time_process(process: subprocess.Popen):
     return endtime - starttime
 
 
-def run_test(test_name: str, program: Path, input_file: Path, output_file: Path, show_all: bool, show_diff: bool):
+def run_test(test_name: str, program: Path, input_file: Path, output_file: Path, show_all: bool, show_diff: bool, regenerate: bool):
     options = output_file.name.split('_')
     if show_all:
         print(f'Running "{test_name}" ({" ".join(options)})...', end=' ', flush=True)
@@ -51,41 +52,45 @@ def run_test(test_name: str, program: Path, input_file: Path, output_file: Path,
             print(f'Test "{test_name}" ({" ".join(options)})', end=' ')
         print(f'FAIL ({elapsed_time:.2f}s, return code is {process.returncode})')
         return False
-    with open(output_file, 'r', encoding='utf-8', newline='') as f:
-        expected_output = f.read()
     actual_output = process.stdout.read().decode('utf-8')
-    if actual_output != expected_output:
-        if not show_all:
-            print(f'Test "{test_name}" ({" ".join(options)})', end=' ')
-        print(f'FAIL ({elapsed_time:.2f}s)')
-        if show_diff:
-            print('\n'.join('\t' + repr(x) for x in difflib.unified_diff(expected_output.split('\n'), actual_output.split('\n'), fromfile=str(output_file), tofile='-', lineterm='')))
-        return False
+    if regenerate:
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(actual_output)
+    else:
+        with open(output_file, 'r', encoding='utf-8', newline='') as f:
+            expected_output = f.read()
+        if actual_output != expected_output:
+            if not show_all:
+                print(f'Test "{test_name}" ({" ".join(options)})', end=' ')
+            print(f'FAIL ({elapsed_time:.2f}s)')
+            if show_diff:
+                print('\n'.join('\t' + repr(x) for x in difflib.unified_diff(expected_output.split('\n'), actual_output.split('\n'), fromfile=str(output_file), tofile='-', lineterm='')))
+            return False
     if show_all:
         print(f'SUCCESS ({elapsed_time:.2f}s)')
     return True
 
 
-def run_tests(main_test_dir: Path, program: Path, test_dir: Path, show_all: bool, show_diff: bool) -> bool:
+def run_tests(main_test_dir: Path, program: Path, test_dir: Path, show_all: bool, show_diff: bool, regenerate: bool) -> bool:
     input_file = test_dir / 'input'
     output_files = sorted(output_file for output_file in test_dir.iterdir() if output_file != input_file)
     success_count = 0
     for output_file in output_files:
-        if run_test(test_dir.name, program, input_file, output_file, show_all, show_diff):
+        if run_test(test_dir.name, program, input_file, output_file, show_all, show_diff, regenerate):
             success_count += 1
     return success_count, len(output_files)
 
 
 def main() -> None:
     main_test_dir = Path(sys.argv[0]).parent
-    program, show_all, show_diff = parse_args()
+    program, show_all, show_diff, regenerate = parse_args()
     
     test_dirs = find_test_dirs(main_test_dir)
     
     success_count = 0
     test_count = 0
     for test_dir in test_dirs:
-        this_success_count, this_test_count = run_tests(main_test_dir, program, test_dir, show_all, show_diff)
+        this_success_count, this_test_count = run_tests(main_test_dir, program, test_dir, show_all, show_diff, regenerate)
         success_count += this_success_count
         test_count += this_test_count
     print(f'Passed {success_count}/{test_count} tests.')
