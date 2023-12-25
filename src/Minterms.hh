87a67662fcde7b959c9aa308d3b4fca6b3c68a7f
Piotr Siupa <piotr.siupa+karnaugh@gmail.com>
2023-12-25 18:18:26 +0100

    Fixed a mayor bug in how minterms are counted
    Basically, adding a minterm that already exist or removing one that
    doesn't exits was still changing the count.
    
    Also, added a method `Minterms.isEmpty()`.

--- src/Minterms.hh
+++ src/Minterms.hh
@@ -37,12 +37,13 @@ public:
 	[[nodiscard]] bool operator==(const Minterms &other) const { return this->bitset == other.bitset; }
 	[[nodiscard]] bool operator!=(const Minterms &other) const { return this->bitset != other.bitset; }
 	
+	[[nodiscard]] bool isEmpty() const { return size == 0; }
 	[[nodiscard]] std::size_t getSize() const { return size; }
 	[[nodiscard]] bool check(const Minterm minterm) const { return bitset[minterm]; }
 	duplicates_t findDuplicates(const Minterms &other) const { duplicates_t duplicates; std::set_intersection(this->cbegin(), this->cend(), other.cbegin(), other.cend(), std::back_inserter(duplicates)); return duplicates; }
-	bool add(const Minterm minterm) { const bool previous = check(minterm); if (!previous) bitset[minterm] = true; ++size; return !previous; }
+	bool add(const Minterm minterm) { const bool previous = check(minterm); if (!previous) { bitset[minterm] = true; ++size; } return !previous; }
 	void add(const Minterms &other, const std::size_t duplicateCount) { std::transform(other.bitset.begin(), other.bitset.end(), this->bitset.begin(), this->bitset.begin(), std::logical_or<bool>()); this->size += other.size - duplicateCount; }
-	bool remove(const Minterm minterm) { const bool previous = check(minterm); if (previous) bitset[minterm] = false; --size; return previous; }
+	bool remove(const Minterm minterm) { const bool previous = check(minterm); if (previous) { bitset[minterm] = false; --size; } return previous; }
 	
 	ConstIterator begin() const { ConstIterator iter{*this, 0}; if (!bitset.empty() && !bitset[0]) ++iter; return iter; }
 	ConstIterator end() const { return {*this, bitset.size()}; }
