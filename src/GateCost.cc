27fbd9d0a6ca02131b6908fcb49e5a91a107199d
Piotr Siupa <piotr.siupa+karnaugh@gmail.com>
2024-02-18 13:18:16 +0100

    Added cost without input NOTs to `gate-costs`
    Another thing that may be helpful for development.

--- src/GateCost.cc
+++ src/GateCost.cc
@@ -8,6 +8,7 @@ void GateCost::printGateCost(std::ostream &o, const bool full) const
 	const auto orCount = getOrCount();
 	o << "Gate cost: NOTs = " << notCount << ", ANDs = " << andCount << ", ORs = " << orCount;
 	if (full)
-		o << ", overall cost = " << getCost(notCount, andCount, orCount);
+		o << ", overall cost = " << getCost(notCount, andCount, orCount)
+				<< ", cost without input NOTs = " << getCost(andCount, orCount);
 	o << '\n';
 }
